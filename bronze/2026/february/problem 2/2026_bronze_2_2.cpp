6ec17e202caf5c0e19c6bcc16d942f5411612795
Ethan Sun <ethan.sun930@gmail.com>
2026-03-08 19:00:20 -0700

    more code

--- bronze/2026/february/problem 2/2026_bronze_2_2.cpp
+++ bronze/2026/february/problem 2/2026_bronze_2_2.cpp
@@ -2,71 +2,45 @@
 #include<vector>
 #include<string>
 using namespace std;
-// vector<int> base10(long long n) {
-//     vector<int> ans;
-//     while (n > 0) {
-//         ans.push_back(n%10);
-//         n /= 10;
-//     }
-//     vector<int> reverse_ans(ans.size());
-//     for (int i = 0; i < ans.size();) {
-//         reverse_ans[i] = ans[ans.size() - i - 1];
-//     }
-//     return reverse_ans;
-// }
-// long long turn_back(vector<int> representation) {
-//     long long n = 0;
-//     for (int i = 0; i < representation.size(); i++) {
-//         n += 10^i * representation[i];
-//     }
-//     return n;
-// }
-int main() {
+#define KMOD 1000000007
+vector<long long> find(void) {
+    vector<long long> f;
+    f.push_back(1);
+    f.push_back(3);
+    for (int i = 2; i <= 200000; i++) {
+        f.push_back((f.back() * 2)%KMOD);
+    }
+    return f;
+}
+int main(void) {
+    vector<long long> f = find();
     int T;
     cin >> T;
     vector<long long> answers;
-    for(int i = 0; i < T; i++) {
+    for (int k = 0; k < T; k++) {
         string x;
         cin >> x;
         long long ans = 0;
-        while (x != "0") {
-            bool zeros_and_ones = true;
-            for (const char&j : x) {
-                if (j != '0' && j != '1') {
-                    zeros_and_ones = false;
-                }
+        bool zeros_and_ones = true;
+        for (int i = 0; i < x.size(); i++) {
+            char curr_digit = x[i];
+            if (curr_digit != '0' && curr_digit != '1') {
+                zeros_and_ones = false;
             }
-            if (zeros_and_ones == true) {
-                int amount = 0;
-                while(x[x.length() - amount - 1] == '0') {
-                    amount++;
-                }
-                if(amount = 0) {
-                    if (x[x.length() - 1] == '0') {}
-                }
-                else {
-                    for (int i = 0; i < amount; i++) {
-                        x[x.length() - amount - 1] = '9';
-                    }
-                }
+            if (curr_digit % 2 == 1) {
+                long long tmp = f[x.size() - 1 - i];
+                // cout << tmp << " " << x.size() - 1 - i  << " " << i << "\n";
+                ans += tmp;
+                ans %= KMOD;
             }
-            else {
-                for (int j = 0; j < x.size(); j++) {
-                    if (x[j] = '0' || x[j] = '2' || x[j] = '4' || x[j] = '6' || x[j] = '8') {
-                        x[j] = '0';
-                    }
-                    else {
-                        x[j] = '1';
-                    }
-                }
-            }
+        }
+        if (zeros_and_ones == false) {
             ans++;
-            ans %= 10^9 + 7;
+            ans %= KMOD;
         }
         answers.push_back(ans);
     }
-    for (const int& ans : answers) {
-        cout << ans << "\n";
+    for (const long long& ans : answers) {
+        cout << ans << '\n';
     }
-    return 0;
 }
