982c13899ef619aec0c972b141d00a5a47933cfe
Ethan Sun <ethan.sun930@gmail.com>
2026-03-13 20:12:44 -0700

    kfjlds

--- bronze/2026/february/problem 3/2026_bronze_2_3.cpp
+++ bronze/2026/february/problem 3/2026_bronze_2_3.cpp
@@ -2,11 +2,12 @@
 #include<string>
 #include<vector>
 #include<utility>
+#include<unordered_set>
 using namespace std;
 int main() {
     int T;
     cin >> T;
-    vector<vector<int>> movements;
+    vector<vector<int>> movements(T);
     vector<int> answers;
     for (int k = 0; k < T; k++) {
         int N;
@@ -15,9 +16,13 @@ int main() {
         string favorite;
         cin >> favorite;
         int ans;
-        vector<pair<int, int>> indexes[26];
+        unordered_set<pair<int, int>> indexes[26];
         string first;
         cin >> first;
+        unordered_set<int> first_indexes[26];
+        for (int i = 0; i < M; i++) {
+            first_indexes[first[i] - 'a'].push_back(i);
+        }
         for (int i = 1; i < N; i++) {
             string a;
             cin >> a;
@@ -27,15 +32,20 @@ int main() {
         }
         for (int i = 0; i < M; i++) {
             if (first[i] == favorite[i]) {
+                first_indexes[first[i] - 'a'].erase(i);
                 continue;
             }
-            for (int j = i + 1; j < M; j++) {
-                if (first[j] == first[i]) {
-                    ans++;
-                    movements.push_back({1, 1, i, j});
-                }
+            else if (first_indexes[favorite[i] - 'a'].size() > 0) {
+                auto a = first_indexes[favorite[i] - 'a'].begin();
+                int val = *a;
+                first_indexes[favorite[i] - 'a'].erase(val);
+                first_indexes[first[i] - 'a'].erase(i);
+                first_indexes[first[i] - 'a'].insert(val);
+                ans++;
+                ansers[k].push_back([1, 1, i, val]);
+                continue;
             }
-            
+            auto a = first_indexes[favorit]
         }
     }
     return 0;
