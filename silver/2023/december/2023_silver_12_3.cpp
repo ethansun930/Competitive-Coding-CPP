11191d0a7af1de590b0be08eeadf863cd9dbfbbc
ethan.sun <ethan.sun930@gmail.com>
2026-08-15 09:16:49 -0700

    hi

--- silver/2023/december/2023_silver_12_3.cpp
+++ silver/2023/december/2023_silver_12_3.cpp
@@ -12,10 +12,10 @@ int main() {
     }
     string S;
     cin >> S;
-    vector<int> com(C, 2);
+    vector<int> com(C, 0);
     for (int i = 0; i < C; i++) {
         if (S[i] == 'L') {
-            com[i] = 0;
+            com[i] = -1;
         } else if (S[i] == 'R') {
             com[i] = 1;
         }
